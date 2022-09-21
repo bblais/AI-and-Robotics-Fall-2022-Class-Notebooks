f52286c565b2274228a8beb8eb900d0a1f5bbb24
Brian Blais <bblais@gmail.com>
2022-09-21 13:58:46 -0400

    misc

--- Sprint #1 - Programming and Games/2022-09-21 Misc.py
+++ Sprint #1 - Programming and Games/2022-09-21 Misc.py
@@ -78,6 +78,52 @@ move=[value,location]
 move
 
 
+# In[15]:
+
+
+state=Board(3,3)
+for loc in [0,1,2]:
+    state[loc]=1
+for loc in [6,7,8]:
+    state[loc]=2
+    
+state
+
+
+# In[16]:
+
+
+state.show_locations()
+
+
+# In[23]:
+
+
+def valid_moves(state,player):
+    
+    moves=[]
+    if player==1:
+        
+        for start in [0,1,2,3,4,5]:
+            if state[start]==1 and state[start+3]==0:
+                moves.append( [start,start+3] )
+            
+#         if state[1]==1 and state[4]==0:
+#             moves.append( [1,4] )       
+            
+#         if state[2]==1 and state[5]==0:
+#             moves.append( [2,5] )       
+            
+            
+    return moves
+
+
+# In[22]:
+
+
+valid_moves(state,1)
+
+
 # In[ ]:
 
 
