db6d568ca1dcd4620e62558eda8729c9c8658f12
Brian Blais <bblais@gmail.com>
2022-11-09 07:57:54 -0500

    arrays and images start

---
+++ Sprint #4 - Images, Classification, and Neural Networks/2022-11-09 Arrays and Images.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[2]:
+
+
+from pylab import *
+
+
+# https://www.theverge.com/2021/4/12/22379880/artificial-intelligence-cat-photos-gan
+# 
+# https://thiscatdoesnotexist.com/
+# 
+# 
+# A cool seminar on colormaps:  https://youtu.be/xAoljeRJ3lU
+
+# In[5]:
+
+
+im=imread('images/cats.jpg')
+
