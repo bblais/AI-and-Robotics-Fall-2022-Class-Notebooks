1d55f19eb467916f8525634f1a5719669a736768
Brian Blais <bblais@gmail.com>
2022-11-19 08:30:26 -0500

    all the rest of read state

---
+++ Sprint #4 - Images, Classification, and Neural Networks/2020-11-19 - Prepping Read State Part 2 - Make Training Board Squares Image Set.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+from pylab import *
+
+
+# Run this script with your board images, then manually put those images into folders named by the type of piece (see the images/all_pieces folder for an example of this)
+
+# In[2]:
+
+
+def make_training_squares(filefilter,shape,locations=None):
+    from glob import glob
+    import os
+    import json
+    
+    if locations is None:
+        with open('locations.json') as json_file:
+            locations = json.load(json_file)        
+    
+    fnames=glob(filefilter)
+    arr=array(imread(fnames[0]))
+    for location in locations:
+        c,r=location
+        c1=int(c-shape[1]/2)
+        c2=int(c+shape[1]/2)
+        r1=int(r-shape[0]/2)
+        r2=int(r+shape[0]/2)
+
+        c2=c2+(shape[1]-(c2-c1))
+        r2=r2+(shape[0]-(r2-r1))
+
+        arr[r1:r2,c1:c2,:]=arr[r1:r2,c1:c2,:]+100
+        
+
+    clf()
+    imshow(arr)
+    base,name=os.path.split(fnames[0])
+    newdirname=base+"/squares"
+    if not os.path.exists(newdirname):
+        os.mkdir(newdirname)
+    
+    for fname in fnames:
+        arr=array(imread(fname))
+        base,name=os.path.split(fname)
+        base,ext=os.path.splitext(name)
+        for i,location in enumerate(locations):
+            c,r=location
+            c1=int(c-shape[1]/2)
+            c2=int(c+shape[1]/2)
+            r1=int(r-shape[0]/2)
+            r2=int(r+shape[0]/2)
+
+            c2=c2+(shape[1]-(c2-c1))
+            r2=r2+(shape[0]-(r2-r1))
+
+            if (r2>arr.shape[0]) or (r1<0) or (c2>arr.shape[1]) or (c1<0):
+                raise IndexError('Square size out of bounds of image.')
+            
+            square=arr[r1:r2,c1:c2,:]
+            
+            newfname=base+"_%d" %(i) + ext
+            print(newdirname+"/"+newfname)
+            imsave(newdirname+"/"+newfname,square)
+
+
+# In[3]:
+
+
+make_training_squares("images/board images/*.jpg",
+                      (40,40))
+
+
+# In[ ]:
+
+
+
+
---
+++ Sprint #4 - Images, Classification, and Neural Networks/2022-11-19 - Full Project Outline.py
@@ -0,0 +1,395 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Let's work backward from the answer we need to how we get there.  We'll write the entire robot playing a board game.  We'll make empty or partial functions first, and then fill them out.
+
+# In[ ]:
+
+
+# don't run these yet -- they are defined below
+state=read_state()     #  read the state from the world
+move=get_move(state,1)   # from an agent
+make_move(move)        # robot motion to move pieces, etc...
+
+
+# # get_move
+
+# In[1]:
+
+
+from breakthrough import *
+
+
+# there are several get_move functions one could use.  Like random....
+
+# In[2]:
+
+
+def random_move(state,player):
+    moves=valid_moves(state,player)
+    return random.choice(moves)
+
+def get_move(state,player):
+    return random_move(state,player)
+
+
+# or minimax the same way.
+
+# In[3]:
+
+
+def get_move(state,player):
+    return minimax_move(state,player)
+
+
+# Q or skittles, the move comes from the table.
+
+# In[4]:
+
+
+def get_move(state,player):
+    if player==1:
+        Q=LoadTable("Q1_breakthrough_table.json")
+    else:
+        Q=LoadTable("Q2_breakthrough_table.json")
+        
+    
+    if state not in Q:
+        return random_move(state,player)
+    else:
+        return top_choice(Q[state])
+
+
+# In[5]:
+
+
+state=initial_state()
+
+
+# In[6]:
+
+
+get_move(state,1)
+
+
+# ## make_move
+
+# the minimal one just prints the move
+
+# In[7]:
+
+
+def make_move(move):
+    print("If I could move, I would do move: ",move)
+
+
+# everything else will be specific to your robot, but it might do something like this.
+
+# In[8]:
+
+
+move=[12,8]
+move=[9,6]
+
+
+# In[9]:
+
+
+def move_forward(distance):
+    print("forward ",distance)
+    
+def move_backward(distance):
+    print("backward ",distance)    
+    
+def turn_robot_left_90():
+    print("left 90")
+    
+def turn_robot_right_90():
+    print("right 90")    
+    
+def turn_robot_left_45():
+    print("left 45")    
+    
+def turn_robot_right_45():
+    print("right 45")     
+    
+def arm_up():
+    print("arm up")
+def arm_down():
+    print("arm down")
+
+
+# In[10]:
+
+
+def make_move(move):
+    print("Making move ",move)
+    
+    board=Board(4,4)  # just to get the conversion functions for free
+    
+    start,end=move
+    rs,cs=board.rc_from_index(start)  # convert to row, column
+    re,ce=board.rc_from_index(end)
+    
+    distance_to_board=10
+    length_column=4
+    length_row=4
+
+    type_of_move=ce-cs  # 0 for a forward move, +1 for a right-hand diagonal, -1 for left-hand diagonal
+    distance_to_column=distance_to_board+length_column*cs
+    distance_to_row=(4-rs)*length_row
+
+    if type_of_move==0:  # forward
+
+        move_forward(distance_to_column)
+        turn_robot_left_90()
+        move_forward(distance_to_row)
+        arm_down()  # to push the piece
+        move_forward( 1*length_row )
+        arm_up()
+
+        # go back
+        move_backward(distance_to_row + 1*length_row)
+        turn_robot_right_90()
+        move_backward(distance_to_column)
+
+    elif type_of_move==1:  # right-hand diagonal
+
+        move_forward(distance_to_column)
+        turn_robot_left_90()
+        move_forward(distance_to_row)
+
+
+        arm_down()  # to push the piece
+        turn_robot_right_45()
+        move_forward( 1*length_row )
+
+        # go back
+        move_backward(1*length_row)
+        turn_robot_left_45()    
+        move_backward(distance_to_row )
+        turn_robot_right_90()
+        move_backward(distance_to_column)
+
+    elif type_of_move==-1:  # right-hand diagonal
+        move_forward(distance_to_column)
+        turn_robot_left_90()
+        move_forward(distance_to_row)
+
+
+        arm_down()  # to push the piece
+        turn_robot_left_45()
+        move_forward( 1*length_row )
+
+        # go back
+        move_backward(1*length_row)
+        turn_robot_right_45()    
+        move_backward(distance_to_row )
+        turn_robot_right_90()
+        move_backward(distance_to_column)
+
+    else:
+        raise ValueError("You can't get there from here.")
+
+    
+
+
+# In[11]:
+
+
+state=initial_state()
+show_state(state)
+state.show_locations()
+
+
+# In[12]:
+
+
+make_move([13,8])
+
+
+# ## read_state
+
+# a nice fall-back is to read the state from a file
+
+# In[13]:
+
+
+def read_state_from_file(filename='current_board.txt'):
+    with open(filename) as fid:
+        text=fid.read()
+
+    text2=text.strip().split('\n')
+    number_of_rows=len(text2)
+    number_of_cols=len(text2[0].split())
+    
+    b=Board(number_of_rows,number_of_cols)
+        
+    board=[int(v) for v in text.split()]
+    b.board=board
+    return b
+
+
+# In[14]:
+
+
+def read_state():
+    state=read_state_from_file()
+    return state
+
+
+# In[15]:
+
+
+with open("current_board.txt","w") as fid:
+    fid.write("""
+2 0 2 0 
+0 2 0 0
+1 0 1 1
+0 1 0 0
+    """)
+
+
+# In[16]:
+
+
+state=read_state()
+show_state(state)
+
+
+# now we can do the reading from the world
+
+# In[17]:
+
+
+from classy import *
+
+
+# Assuming you have collection of training images, train the classifier....
+
+# In[18]:
+
+
+def take_picture(fname):
+    pass
+
+def get_square(arr,index,shape,locations=None):
+    import json
+    
+    if locations is None:
+        with open('locations.json') as json_file:
+            locations = json.load(json_file)        
+    
+    try:
+        location=locations[index]
+    except IndexError:
+        print("locations.json file probably corrupt.")
+        raise 
+        
+    c,r=location
+    c1=int(c-shape[1]/2)
+    c2=int(c+shape[1]/2)
+    r1=int(r-shape[0]/2)
+    r2=int(r+shape[0]/2)
+
+    c2=c2+(shape[1]-(c2-c1))
+    r2=r2+(shape[0]-(r2-r1))
+
+    square=arr[r1:r2,c1:c2,:]
+    
+    return square
+
+
+# In[21]:
+
+
+from pylab import imread,imsave
+
+
+# In[27]:
+
+
+def read_state():
+    from pylab import imread,imsave
+    import os
+
+    # train the classifier
+    images=image.load_images('images/all_pieces',delete_alpha=True)
+    shape=images.data[0].shape[:2]
+    data_train=data=image.images_to_vectors(images,verbose=True)  # train on all of them
+
+    classifier=kNearestNeighbor()
+    classifier.fit(data_train.vectors,data_train.targets)
+
+
+    # get the picture
+    take_picture('current_board.jpg')
+    arr=imread('current_board.jpg')
+
+
+    # slice the picture into squares of the right size
+    shape=data_train.shape[:2]
+    squares=[get_square(arr,i,shape) for i in range(16)]
+    test_images=image.array_to_image_struct(squares)
+
+    # get predictions
+    test_data=image.images_to_vectors(test_images)
+    predictions=classifier.predict(test_data.vectors)
+
+    if not os.path.exists('images/predicted'):
+        os.mkdir('images/predicted')
+    for i,(square,prediction) in enumerate(zip(squares,predictions)):
+        imsave('images/predicted/square %d predicted as %s.jpg' % (i,data_train.target_names[prediction]),square)
+
+    
+    # reconstruct the state from the predictions
+    state=Board(4,4)
+    for i in range(16):
+        color=data_train.target_names[predictions[i]]
+        if color=="white":
+            state[i]=0
+        elif color=="red":
+            state[i]=1
+        elif color=="black":
+            state[i]=2
+        else:
+            raise ValueError("You can't get there from here.")
+
+    print("Current state is:")
+    print(state)
+
+    x=input("""
+    Hit return if this is correct, otherwise type a character 
+    and the state will be read from current_board.txt.""")
+
+    if x:
+        state=read_state_from_file()
+
+    print("Using")
+    print(state)
+
+    
+    return state
+
+
+# In[28]:
+
+
+state=read_state()
+
+
+# ## Now the full game
+
+# In[60]:
+
+
+state=read_state()     #  read the state from the world
+move=get_move(state,1)   # from an agent
+make_move(move)        # robot motion to move pieces, etc...
+
+
+# In[ ]:
+
+
+
+
